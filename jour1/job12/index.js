a8d3b0169493bd470c5bb1b9a99ac0ec47f21a55
william-rauwens-oliver <william.rauwens-oliver@laplateforme.io>
2024-05-13 10:59:21 +0200

    Add job12 Finish.

---
+++ jour1/job12/index.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    
+    fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+        if (err) {
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.end('Erreur interne du serveur');
+            return;
+        }
+
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(data);
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Serveur démarré sur http://localhost:${PORT}`);
+});
