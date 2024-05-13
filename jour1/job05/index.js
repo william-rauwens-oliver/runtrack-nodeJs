a56ef31218f6be0e6eed4d52f88d2c8477852dcf
william-rauwens-oliver <william.rauwens-oliver@laplateforme.io>
2024-05-13 10:01:36 +0200

    Job05 Finish.

--- jour1/job05/index.js
+++ jour1/job05/index.js
@@ -1,16 +1,12 @@
 const path = require('path');
 
-// Récupérer le chemin du fichier en cours d'exécution
 const cheminFichier = __filename;
 
-// Récupérer le nom du fichier
 const nomFichier = path.basename(cheminFichier);
 console.log("Nom du fichier:", nomFichier);
 
-// Récupérer l'extension du fichier
 const extensionFichier = path.extname(cheminFichier);
 console.log("Extension du fichier:", extensionFichier);
 
-// Récupérer le répertoire parent du fichier
 const repertoireParent = path.dirname(cheminFichier);
 console.log("Répertoire parent du fichier:", repertoireParent);
