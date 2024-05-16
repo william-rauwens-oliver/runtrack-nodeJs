12c86ffba7a583dff12fa99aa9b7029605d4a4a8
william-rauwens-oliver <william.rauwens-oliver@laplateforme.io>
2024-05-16 16:51:48 +0200

    Add Job9 Finish.

---
+++ jour4/job9/index.js
@@ -0,0 +1,39 @@
+const readline = require('readline');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/LaPlateforme')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));
+
+const studentSchema = new mongoose.Schema({
+  id: Number,
+  lastname: String,
+  firstname: String,
+  students_number: Number,
+  year_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Year' }
+});
+
+const Student = mongoose.model('Student', studentSchema, 'student');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Enter student ID to delete: ', (studentId) => {
+    Student.deleteOne({ _id: studentId })
+      .then(result => {
+        if (result.deletedCount === 1) {
+          console.log('Student deleted');
+        } else {
+          console.log('Student not found');
+        }
+      })
+      .catch(err => {
+        console.error(err);
+      })
+      .finally(() => {
+        rl.close();
+      });
+  });
+  
