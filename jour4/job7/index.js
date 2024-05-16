2bb0d9f33988185938d83e96a6e3ff4dd56376d3
william-rauwens-oliver <william.rauwens-oliver@laplateforme.io>
2024-05-16 16:45:23 +0200

    Add Job7 Finish.

--- jour4/job7/index.js
+++ jour4/job7/index.js
@@ -13,21 +13,33 @@ const studentSchema = new mongoose.Schema({
   year_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Year' }
 });
 
-const Student = mongoose.model('Student', studentSchema);
+const Student = mongoose.model('Student', studentSchema, 'student');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-rl.question('Entrer le nom de famille : ', (lastname) => {
+rl.question('Enter last name to search: ', (lastname) => {
   Student.find({ lastname })
-    .then((students) => {
-      console.log(students);
-      rl.close();
+    .then(students => {
+      students.forEach(student => {
+        console.log("ID:", student.id);
+        console.log("Last Name:", student.lastname);
+        console.log("First Name:", student.firstname);
+        console.log("Student Number:", student.students_number);
+        console.log("Year ID:", student.year_id);
+        console.log("\n");
+      });
+      if (students.length === 0) {
+        console.log("No student found with the last name:", lastname);
+      }
     })
-    .catch((err) => {
+    .catch(err => {
       console.error(err);
+    })
+    .finally(() => {
       rl.close();
     });
 });
+
---
+++ jour4/job8/index.js
@@ -0,0 +1,40 @@
+const readline = require('readline');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/LaPlateforme')
+  .then(() => console.log('Connecté à MongoDB...'))
+  .catch(err => console.error('Impossible de se connecter à MongoDB...', err));
+
+const studentSchema = new mongoose.Schema({
+  id: Number,
+  lastname: String,
+  firstname: String,
+  students_number: Number,
+  year_id: { type: mongoose.Schema.Types.ObjectId, ref: 'year' }
+});
+
+const Student = mongoose.model('student', studentSchema);
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Entrez l\'identifiant de l\'étudiant à mettre à jour : ', (studentId) => {
+  rl.question('Entrez le nouvel identifiant du cursus : ', (newYearId) => {
+    // Mettre à jour l'étudiant en utilisant son identifiant
+    Student.findByIdAndUpdate(studentId, { year_id: newYearId }, { new: true })
+      .then(updatedStudent => {
+        if (updatedStudent) {
+          console.log('Étudiant mis à jour avec succès :', updatedStudent);
+        } else {
+          console.log('Aucun étudiant trouvé avec cet identifiant.');
+        }
+        rl.close();
+      })
+      .catch(err => {
+        console.error('Erreur lors de la mise à jour de l\'étudiant :', err);
+        rl.close();
+      });
+  });
+});
